7bc18897020fd59bdcf6eb8969c8d7153693f77d
GODDONGHYUN <leenl1168@gmail.com>
2024-03-20 11:53:55 +0900

    2024-03-20 JAVASCRIPT 배열 순회, 배열 메서드, 배열 고차함수 개념

--- js/JS 7-10/10.JS_Array/array2.js
+++ js/JS 7-10/10.JS_Array/array2.js
@@ -1,16 +1,42 @@
 /*
     배열 고차 함수
-    - 
+    - 배열의 불변성을 최대한 보장하여 부수효과를 제거한 메서드
 */
-const array = [7, 11, 20, 1, 8, 3, 16, 2, 19, 9, 10, 4, 13, 12, 14, 17, 15, 5, 18, 6];
+const array = [
+  7, 11, 20, 1, 8, 3, 16, 2, 19, 9, 10, 4, 13, 12, 14, 17, 15, 5, 18, 6,
+];
 
 /*
     sort(비교 함수) : 
-    - 
+    - 배열의 요소를 적절하게 정렬
 */
+array.sort();
+console.log(array);
 
+const asc = (x, y) => {
+  return x - y;
+};
+array.sort(asc);
+console.log(array);
 
-console.log('==============================');
+array.sort(function (x, y) {
+  return y - x;
+});
+console.log(array);
+
+const objects = [
+  { x: 9, y: -5 },
+  { x: -2, y: -0 },
+  { x: 0, y: -3 },
+  { x: 6, y: 5 },
+];
+
+console.log(objects);
+objects.sort(function (a, b) {
+  return a.x - b.x;
+});
+console.log(objects);
+console.log("==============================");
 
 /*
     forEach(콜백 함수) : 
@@ -20,8 +46,7 @@ console.log('==============================');
     - 
 */
 
-
-console.log('==============================');
+console.log("==============================");
 
 /*
     map(콜백 함수) : 
@@ -29,8 +54,7 @@ console.log('==============================');
     - 
 */
 
-
-console.log('==============================');
+console.log("==============================");
 
 /*
     filter(콜백 함수) : 
@@ -38,19 +62,16 @@ console.log('==============================');
     - 
 */
 
-
-console.log('==============================');
-
-/*
-    find(value, index) : 
-*/
-
-
-console.log('==============================');
+console.log("==============================");
 
 /*
     find(value, index) : 
 */
 
+console.log("==============================");
 
-console.log('==============================');
+/*
+    find(value, index) : 
+*/
+
+console.log("==============================");
